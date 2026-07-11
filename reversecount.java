a2fc26faa2b8599fd34cee8b1585f817178538ea
shubhamYadav-28 <shubhamraosahab17@gmail.com>
2026-07-11 14:25:41 +0530

    Add files via upload

---
+++ reversecount.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class reversecount {
+    public static void main(String[] args) {
+        // for(int i=10;i>0;i--){
+        //     System.out.println(i);
+        int num=10;
+        while(num>0){
+             if(num == 5) {
+                num--;
+                continue;}
+            System.out.println(num);
+            num--;
+           
+        
+        }
+        }
+    }
+    
+
