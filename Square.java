ce63b1bed47fe923231616b4ef1d9d95727527f9
shubhamYadav-28 <shubhamraosahab17@gmail.com>
2026-07-11 14:25:56 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Square {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for(int i = 0; i<num; i++){
+            for(int j = 0; j<num; j++){
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+}
