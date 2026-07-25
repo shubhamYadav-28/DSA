e7daaf72820482c1a3aafb08508dc42d90130708
shubhamYadav-28 <shubhamraosahab17@gmail.com>
2026-07-25 14:30:17 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,13 @@
+package Searching;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+    int[] arr={ 12,34,5,7,89};
+    for(int i=0;i<arr.length;i++){
+        
+        if(arr[i]==5){
+            System.out.println(i);
+        }
+    }
+    }
+}
