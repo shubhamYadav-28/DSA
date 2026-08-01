7523b7591771eb1034334d4893719e3727fa7fac
shubhamYadav-28 <shubhamraosahab17@gmail.com>
2026-08-01 14:28:46 +0530

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,4 @@
-package linkedList;
-
-public class LinkedList {
+public class LinkedList{
     class Node{
         int data;
         Node next;
@@ -10,18 +8,80 @@ public class LinkedList {
             this.next = null;
         }
     }
-    public static void main(String[] args) {
-        LinkedList List = new LinkedList();
-        Node head= List. new Node(10);
-        head.next = List.new Node(20);
-        head.next.next = List.new Node(30);
-        head.next.next.next = List.new Node(40);
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-       
+    Node head;
+    public void insertAtHead(int value){
+        Node newNode = this.new Node(value);
+        newNode.next = head;
+        head = newNode;
     }
-    
-}
+    public void insertAtTail(int value){
+        Node newNode = this.new Node(value);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while(current.next !=null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    public void printList(){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + "->");
+            curr =curr.next;
+        }
+        System.out.println("null");
+    }
+    public int getValue(int index){
+        Node current = head;
+        int count = 0;
+        while(current != null){
+            if(count == index){
+                return current.data;
+            }
+            count++;
+            current = current.next;
+        }
+        return -1;
+    }
+    public int getSize(){
+        Node current = head;
+        int count = 0;
+        while(current != null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    public boolean search(int value){
+        Node current = head;
+        while(current !=null){
+            if(current.data == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+    public void deleteAtHead(){
+        if(head == null){
+            return;
+        }
+        head = head.next;
+        
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
+        list.printList();
+        System.out.println(list.getValue(2));
+        System.out.println(list.getSize());
+        System.out.println(list.search(10));
+        list.deleteAtHead();
+        list.printList();
+    } 
 }
