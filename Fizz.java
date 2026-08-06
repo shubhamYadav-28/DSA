94f84b1b4d8d1817dbb91c8828c6abc099d9d5de
shubhamYadav-28 <shubhamraosahab17@gmail.com>
2026-08-06 22:20:44 +0530

    Add files via upload

--- Fizz.java
+++ Fizz.java
@@ -1,26 +1,31 @@
+
+import java.util.*;
 public class Fizz {
+    
     public static void main(String[] args) {
         int[] arr=new int [15];
         for(int i =0;i<arr.length;i++){
             arr[i]=i+1;
             if(arr[i]%3==0 && arr[i]%5==0){
-                // System.out.print("fizzbuzz ");
-                return ;
+                 System.out.print("fizzbuzz "+" ");
+                
             }
             else if(arr[i]%3==0){
-                // System.out.print("fizz ");
-                return;
+                System.out.print("fizz "+" ");
+                
             }
             else if(arr[i]%5==0){
-                // System.out.print("buzz ");
-                return;
+                System.out.print("buzz "+" ");
+                
             }
             else{
                 
-                System.out.print(arr[i]);
+                System.out.print(arr[i]+" ");
             }
 
-        }System.out.println(arr);
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(arr));
     }
     
 }
